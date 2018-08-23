902685a497cfceb81c26c008183e55795a8abd27
Andy <andy@hydrogenplatform.com>
2018-08-23 11:15:36 -0400

    Updating to new stuff

--- src/resolverComponents/index.js
+++
@@ -1,9 +0,0 @@
-import ResolverView from './0x1784b4ffc7c507a1110947848F6D32C6122E0bdD'
-import HydroKYCView from './0xC73564202A2392337F37bCE8326F21A98500745a'
-
-let components = {
-  '0x1784b4ffc7c507a1110947848F6D32C6122E0bdD': ResolverView,
-  '0xC73564202A2392337F37bCE8326F21A98500745a': HydroKYCView
-}
-
-export default components
