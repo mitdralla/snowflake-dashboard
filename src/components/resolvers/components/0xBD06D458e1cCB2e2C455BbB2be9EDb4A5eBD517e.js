b28bc61560f396e7337d5f4ad3a31bd65105308b
Andy <andy@hydrogenplatform.com>
2018-09-28 15:02:43 -0400

    Changed Button Logic

--- src/components/resolvers/components/0xBD06D458e1cCB2e2C455BbB2be9EDb4A5eBD517e.js
+++ src/components/resolvers/components/0xBD06D458e1cCB2e2C455BbB2be9EDb4A5eBD517e.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import WyreVerification from 'wyre-react-library';
+import Button from '@material-ui/core/Button';
 import { withWeb3 } from 'web3-webpacked-react';
 
 class WyreVerificationView extends Component {
@@ -11,11 +12,18 @@ class WyreVerificationView extends Component {
   }
 
   render() {
+    const myButton = OurButton
     const apiKey  = "AK-PXV2BMYR-QHCMMQUQ-8YN98EEL-WMCZ3UQ7";
     return (
-      <WyreVerification apiKey={apiKey}/>
+      <WyreVerification myButton={myButton} apiKey={apiKey}/>
     ); }
 
+}
+
+const OurButton = (props) => {
+  return (
+    <Button {...props} variant="contained" color="primary">Open Wyre</Button>
+  );
 }
 
 export default withWeb3(WyreVerificationView);
