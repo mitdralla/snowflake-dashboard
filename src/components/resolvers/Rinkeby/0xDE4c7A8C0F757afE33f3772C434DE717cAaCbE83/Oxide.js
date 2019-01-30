b93a7bea485208f3244cf2f0e4e654680ad77220
Sam Gosling <samuel.jj.gosling@gmail.com>
2019-01-30 22:03:29 +0100

    Leaderboard functionality, grid solution and parsing functions

--- src/components/resolvers/Rinkeby/0xDE4c7A8C0F757afE33f3772C434DE717cAaCbE83/Oxide.js
+++ src/components/resolvers/Rinkeby/0xDE4c7A8C0F757afE33f3772C434DE717cAaCbE83/Oxide.js
@@ -6,7 +6,13 @@ import Avatar from '@material-ui/core/Avatar'
 import OxideIcon from '@material-ui/icons/InvertColors';
 import TimerIcon from '@material-ui/icons/HourglassEmpty';
 import UsersIcon from '@material-ui/icons/People';
+import { withStyles } from '@material-ui/core/styles';
 import StarIcon from '@material-ui/icons/StarBorder';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 import { useAccountEffect, useWeb3Context } from 'web3-react/hooks'
 
@@ -14,6 +20,20 @@ import { useHydroId, useEINDetails, useSnowflakeBalance, useGenericContract, use
 import TransactionButton from '../../../common/TransactionButton'
 
 import { ABI } from './index'
+
+import './Oxide.css'
+
+const blankLeaderboard = { ein: [], id: [], wager: [] , roll: [], oxide: [] }
+
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: '#0971f5',
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
 
 export default function Oxide ({ ein }) {
   const context = useWeb3Context()
@@ -25,76 +45,114 @@ export default function Oxide ({ ein }) {
   const [committedWager, setWager]  = useState(0)
   const [activePunters, setPunters]  = useState(0)
   const [hydroId, hydroIdAddress] = useHydroId()
-  const [leaderboardData, setLeaderboard]  = useState('')
+  const [waitForRender, setInit]  = useState(false)
+  const [leaderboardData, setLeaderboard]  = useState(blankLeaderboard)
   const clientRaindropContract = useNamedContract('clientRaindrop')
   const oxideContract = useGenericContract('0xDE4c7A8C0F757afE33f3772C434DE717cAaCbE83', ABI)
   const snowflakeBalance = useSnowflakeBalance(ein)
 
-  function refreshLeaderboard() {
+  function refreshLeaderboard()  {
+    setLeaderboard(blankLeaderboard)
     oxideContract.getPastEvents("scoreLog", { fromBlock: 0, toBlock: 'latest' })
-      .then(result => {
-        var leaderboard = { account: [], wager: [] , roll: [] }
+    .then((result) => {
+        var leaderboard = blankLeaderboard
         for(var x = 0; x < result.length; x++){
-          if(JSON.stringify(result[x].args.wagerRound) === activeRound){
-            leaderboard.account.push(JSON.stringify(result[x].args.wagerUser))
-            leaderboard.wager.push(JSON.stringify(result[x].args.wagerAmount))
-            leaderboard.roll.push(JSON.stringify(result[x].args.wagerRoll))
+          var round = parseInt(parseObject(result[x].returnValues.wagerRound))
+          if(round == activeRound){
+            leaderboard.ein.push(parseObject(result[x].returnValues.wagerUser))
+            leaderboard.wager.push(parseNumber(parseObject(result[x].returnValues.wagerAmount)))
+            leaderboard.roll.push(parseNumber(parseObject(result[x].returnValues.wagerRoll)))
           }
         }
         setLeaderboard(leaderboard)
       })
-      .catch(() => {
-        setLeaderboard('Error cannot get leaderboard data')
-      })
+      setInit(true)
+  }
+
+  function parseObject(_object) {
+      return JSON.stringify(_object).replace(/["]+/g, '')
+  }
+
+  function parseNumber(_value) {
+      return _value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
   }
 
   useAccountEffect(() => {
       oxideContract.methods.oxideBalance(context.account).call()
-      .then((oxide) => setOxide(oxide.toString()
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")))
+      .then((oxide) => setOxide(parseNumber(oxide)))
       oxideContract.methods.getParticipants().call()
       .then((punters) => setPunters(punters))
       oxideContract.methods.getRound().call()
       .then(round => setRound(round))
       oxideContract.methods.getPot().call()
       .then(pot => activePot(pot))
+      refreshLeaderboard()
   })
 
+    return (
 
-return (
-    <div>
-         <Grid container spacing={12}>
-           <Grid item xs>
+  <div>
+
+         <Grid container direction="row" justify="center" alignItems="center" className="OxideStats">
+           <Grid item >
            </Grid>
-           <Grid item xs={3}>
+           <Grid item >
            <Chip
              avatar={<Avatar><UsersIcon/></Avatar>}
              color='primary'
              label={activePunters}
            />
            </Grid>
-           <Grid item xs={3}>
+           <Grid item >
            <Chip
              avatar={<Avatar><StarIcon/></Avatar>}
              color='primary'
              label={activePot}
            />
            </Grid>
-           <Grid item xs={3}>
+           <Grid item >
            <Chip
              avatar={<Avatar><TimerIcon/></Avatar>}
              color='primary'
              label={activeRound}
            />
            </Grid>
-           <Grid item xs>
+           <Grid item >
            </Grid>
         </Grid>
 
-         <Grid container spacing={12}>
-         <Grid item xs>
+
+        <Grid container direction="row" justify="center" alignItems="center"  className="OxideLeaderboard">
+        <Grid item >
+          <Table>
+                 <TableHead>
+                   <TableRow>
+                     <CustomTableCell>Score</CustomTableCell>
+                     <CustomTableCell align="right">EIN</CustomTableCell>
+                     <CustomTableCell align="right">HydroId</CustomTableCell>
+                     <CustomTableCell align="right">Wager</CustomTableCell>
+                     <CustomTableCell align="right">Roll</CustomTableCell>
+                   </TableRow>
+                 </TableHead>
+                 <TableBody>
+                     <TableRow>
+                       <CustomTableCell component="th" scope="row">
+                         {leaderboardData.ein[0]}
+                       </CustomTableCell>
+                       <CustomTableCell align="right">{leaderboardData.id[0]}</CustomTableCell>
+                       <CustomTableCell align="right">{leaderboardData.wager[0]}</CustomTableCell>
+                       <CustomTableCell align="right">{leaderboardData.roll[0]}</CustomTableCell>
+                       <CustomTableCell align="right">{leaderboardData.oxide[0]}</CustomTableCell>
+                     </TableRow>
+                 </TableBody>
+               </Table>
+               </Grid>
+               </Grid>
+
+         <Grid container direction="row" justify="center" alignItems="center"  className="OxideWager">
+         <Grid item >
          </Grid>
-         <Grid item xs={3}>
+         <Grid item >
          <Chip
            avatar={
              <Avatar>
@@ -106,7 +164,7 @@ return (
            label={snowflakeBalance}
          />
          </Grid>
-         <Grid item xs={3}>
+         <Grid item>
          <Chip
            avatar={
              <Avatar>
@@ -117,14 +175,14 @@ return (
            label={oxideBalance}
          />
          </Grid>
-         <Grid item xs>
+         <Grid item >
          </Grid>
          </Grid>
 
-         <Grid container spacing={12}>
-           <Grid item xs={3}>
+         <Grid container direction="row" justify="center" alignItems="center">
+           <Grid item >
            </Grid>
-           <Grid item xs={6}>
+           <Grid item >
            <TextField
              label="Wager Amount"
              helperText="Disclaimer: You are placing a bet and could possibly lose your funds."
@@ -134,25 +192,25 @@ return (
              halfWidth
            />
            </Grid>
-           <Grid item xs={3}>
+           <Grid item >
            </Grid>
          </Grid>
 
-         <Grid container spacing={12}>
-           <Grid item xs={3}>
+         <Grid container direction="row" justify="center" alignItems="center"  className="OxideButton">
+           <Grid item >
            </Grid>
-           <Grid item xs={3}>
+           <Grid item >
            </Grid>
-           <Grid item xs={3}>
+           <Grid item >
          <TransactionButton
            readyText='Wager'
            method={() => oxideContract.methods.placeWager(committedWager)}
            onConfirmation={context.forceAccountReRender}
           />
           </Grid>
-          <Grid item xs={3}>
+          <Grid item >
           </Grid>
         </Grid>
-       </div>
+   </div>
   )
 }
