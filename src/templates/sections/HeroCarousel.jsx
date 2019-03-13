c7aedb38d4b4ebfb987c1328e2d3812b447fd98e
Timothy Allard <timallard@gmail.com>
2019-03-13 12:00:37 -0500

    hero edits (#23)

--- src/templates/sections/HeroCarousel.jsx
+++ src/templates/sections/HeroCarousel.jsx
@@ -15,6 +15,16 @@ import '../../../node_modules/react-owl-carousel2/src/owl.carousel.css';
 import '../../../node_modules/react-owl-carousel2/src/owl.theme.green.css';
 import '../../../node_modules/react-owl-carousel2/src/owl.carousel.js';
 
+// Placeholder Images
+import demoBG1 from './../../assets/img/angry_birds_logo.jpg'
+import demoBG2 from './../../assets/img/paypal_logo.jpg'
+import demoBG3 from './../../assets/img/cryptokitties_logo.jpg'
+import demoBG4 from './../../assets/img/firefox_logo.png'
+import demoBG5 from './../../assets/img/subway_surfers_logo.png'
+import demoBG6 from './../../assets/img/uber_logo.jpg'
+import demoBG7 from './../../assets/img/uno_logo.jpg'
+import demoBG8 from './../../assets/img/temple_run_logo.png'
+
 import { useHydroBalance, useAccountBalance, useEtherscanLink, useNamedContract } from '../../common/hooks'
 import { Nav, Navbar, NavDropdown, Form, FormControl, Jumbotron, Button, Grid, Container, Row, Column } from 'react-bootstrap'
 
@@ -67,19 +77,65 @@ const styles = theme => ({
   }
 })
 
+var slideBG1 = {
+  backgroundImage: "url(" + demoBG1 + ")"
+};
+
+var slideBG2 = {
+  backgroundImage: "url(" + demoBG2 + ")"
+};
+
+var slideBG3 = {
+  backgroundImage: "url(" + demoBG3 + ")"
+};
+
+var slideBG4 = {
+  backgroundImage: "url(" + demoBG4 + ")"
+};
+
+var slideBG5 = {
+  backgroundImage: "url(" + demoBG5 + ")"
+};
+
+var slideBG6 = {
+  backgroundImage: "url(" + demoBG6 + ")"
+};
+
+var slideBG7 = {
+  backgroundImage: "url(" + demoBG7 + ")"
+};
+
+var slideBG8 = {
+  backgroundImage: "url(" + demoBG8 + ")"
+};
+
 function HeroCarousel({ classes }) {
   return (
     <OwlCarousel options={options} events={events} >
-      <div className="item dAppCarouselItem"><img src="" alt=""/></div>
-      <div className="item dAppCarouselItem"><img src="" alt=""/></div>
-      <div className="item dAppCarouselItem"><img src="" alt=""/></div>
-      <div className="item dAppCarouselItem"><img src="" alt=""/></div>
-      <div className="item dAppCarouselItem"><img src="" alt=""/></div>
-      <div className="item dAppCarouselItem"><img src="" alt=""/></div>
-      <div className="item dAppCarouselItem"><img src="" alt=""/></div>
-      <div className="item dAppCarouselItem"><img src="" alt=""/></div>
-      <div className="item dAppCarouselItem"><img src="" alt=""/></div>
-      <div className="item dAppCarouselItem"><img src="" alt=""/></div>
+      <div className="item dAppCarouselItem" style={slideBG1}>
+        <div className=""></div>
+      </div>
+      <div className="item dAppCarouselItem" style={slideBG2}>
+        <div className=""></div>
+      </div>
+      <div className="item dAppCarouselItem" style={slideBG3}>
+        <div className=""></div>
+      </div>
+      <div className="item dAppCarouselItem" style={slideBG4}>
+        <div className=""></div>
+      </div>
+      <div className="item dAppCarouselItem" style={slideBG5}>
+        <div className=""></div>
+      </div>
+      <div className="item dAppCarouselItem" style={slideBG6}>
+        <div className=""></div>
+      </div>
+      <div className="item dAppCarouselItem" style={slideBG7}>
+        <div className=""></div>
+      </div>
+      <div className="item dAppCarouselItem" style={slideBG8}>
+        <div className=""></div>
+      </div>
     </OwlCarousel>
   )
 }
