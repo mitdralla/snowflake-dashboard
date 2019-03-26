79e3da81db1c0b7576150672dcef50c08e896f78
Timothy Allard <timallard@gmail.com>
2019-03-26 08:48:52 -0500

    sidebar component, dashboard cleanup, index.css cleanup

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,23 +1,19 @@
 import React, { Suspense, lazy } from 'react';
 import { withRouter, Switch, Route, Redirect } from 'react-router'
-import { Link } from 'react-router-dom'
-import * as IonIcons from 'react-icons/io'
 import { useEIN, useHydroId, useEINDetails, useNamedContract } from '../../common/hooks'
-
 import './Dashboard.css'
-import config from '../../config.jsx'
+
 import { Header } from '../Header'
 import { HeroCarousel } from '../HeroCarousel'
+import { Sidebar } from '../Sidebar'
 import { Footer } from '../Footer'
-import SnowflakeHeader from '../SnowflakeHeader'
 
-const sidebarNavItems = config.dappCategories.categories;
+import SnowflakeHeader from '../SnowflakeHeader'
 const NoSnowflakeProvider = lazy(() => import('../states/NoSnowflakeProvider'))
 const NoEIN = lazy(() => import('../states/NoEIN'))
 const NoHydroId = lazy(() => import('../states/NoHydroId'))
 const RouteTabs = lazy(() => import('../RouteTabs'))
 const FinalizeClaim = lazy(() => import('../routes/FinalizeClaim'))
-const DAppCategories = lazy(() => import('../../pages/DAppCategories'))
 
 export default withRouter((function App ({ classes, location }) {
   const ein = useEIN()
@@ -74,29 +70,7 @@ export default withRouter((function App ({ classes, location }) {
       <Header />
       <HeroCarousel />
       <div className="bodyWrapper">
-        <div className="sidebar">
-          <div className="container">
-            <div className="row">
-              <div className="column">
-              <ul className="dappSidebarAuxNav">
-                <Link to="/wallet"><li>dApp Store Wallet</li></Link>
-                <Link to="/your-dapps"><li>Your Added dApps</li></Link>
-                <Link to="/identity"><li>Manage Your Identity (EIN)</li></Link>
-                <Link to="/submit"><li>Submit Your dApp</li></Link>
-              </ul>
-              <h2 className="text-grey">Categories</h2>
-              <ul className="dappSidebarCategoryNav">
-                {sidebarNavItems.map((category, i) => {
-                  const iconString = category.icon;
-                  const icon = React.createElement(IonIcons[iconString]);
-
-                  return <Link to={'/dapps/category/'+category.link} key={i} component={DAppCategories}><li data-category={category.name.replace(/\s+/g, '-').toLowerCase()} key={i}>{icon} {category.name}</li></Link>
-                })}
-              </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Sidebar />
 
         <div className="bodyContent">
           <SnowflakeHeader ein={ein} hydroId={hydroId} />
